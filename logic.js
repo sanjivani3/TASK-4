06fe090841543b91802e18d443ccb4fd7e821351
sanjivani3 <137819545+sanjivani3@users.noreply.github.com>
2023-07-23 18:41:00 +0530

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,27 @@
+# LGMVIP-04
+
+let screen=document.getElementById('screen');
+buttons=document.querySelectorAll('button');
+let screenValue='';
+for(item of buttons){
+    item.addEventListener('click',(e)=> {
+        buttonText=e.target.innerText;
+        console.log('Button text is',buttonText)
+        if(buttonText=='x'){
+            buttonText='*';
+            screenValue== buttonText;
+            screenValue=screenValue;
+        }
+        else if(buttonText=='C'){
+            screenValue='';
+            screenValue= screenValue;
+        }
+        else if(buttonText=='='){
+            screen.value = eval(screenValue);
+        }
+        else{
+            screenValue== buttonText;
+            screenValue= screenValue;
+        }
+    } )
+}
